f658cb5b7a56983bb98a634d089ab5743b06f4dd
hasan <hsnnkb.dev@gmail.com>
2023-04-23 23:22:19 +0100

    Fix folder structure

--- mysite/test_sessions/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Session
-class SessionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Session
-        fields = ["Title", "Description", "Candidateid", "CandidateName", "Date", "LocationName", "Latitude", "Longitude"]
--- mysite/test_sessions/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.conf.urls import url
-from django.urls import path, include
-from .views import ( SessionListApiView )
-
-urlpatterns = [
-    path('sessions/',  SessionListApiView.as_view()),
-]
